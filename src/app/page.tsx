572c15f158307234bd78dc981aec4b85f1687437
Maschkov Konstantin <maschkovc@yandex.ru>
2023-11-21 18:27:36 +0300

    map Test 1

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import './mapContainer.scss';
+
+import Map from '../components/Map'
+
+
+export default function Home() {
+	return (
+		<div className='mapContainer'>
+			<Map />
+		</div>
+	);
+}
